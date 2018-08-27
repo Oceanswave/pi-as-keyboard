10ea0cb52044c1c956a22a8acc9c8a81d0ffd932
Sean McLellan <Oceanswave@users.noreply.github.com>
2018-08-27 13:02:11 -0400

    Update enable_hid.sh

--- enable_hid.sh
+++ enable_hid.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 # this is a stripped down version of https://github.com/ckuethe/usbarmory/wiki/USB-Gadgets - I don't claim any rights
 
-modprobe libcomposite
+#modprobe libcomposite
 cd /sys/kernel/config/usb_gadget/
-mkdir -p g1
-cd g1
-echo 0x1d6b > idVendor # Linux Foundation
+mkdir -p SleepyMatrixController
+cd SleepyMatrixController
+echo 0x1d6b > idVendor # 0x1d6b = Linux Foundation 0x45e = Microsoft
 echo 0x0104 > idProduct # Multifunction Composite Gadget
 echo 0x0100 > bcdDevice # v1.0.0
 echo 0x0200 > bcdUSB # USB2
+
 mkdir -p strings/0x409
-echo "fedcba9876543210" > strings/0x409/serialnumber
-echo "Valentin Brosseau" > strings/0x409/manufacturer
-echo "Keyboard-dev" > strings/0x409/product
-N="usb0"
-mkdir -p functions/hid.$N
+echo "5eaf00d0b57ac1e > strings/0x409/serialnumber
+echo "BaristaLabs, LLC" > strings/0x409/manufacturer
+echo "SleepyMatrix Controller" > strings/0x409/product
+
+mkdir -p configs/c.1/strings/0x409
+echo "Config 1: ECM network" > configs/c.1/strings/0x409/configuration
+echo 250 > configs/c.1/MaxPower
+
+# Add functions here
+
+#keyboard
+mkdir -p functions/hid.usb0
 echo 1 > functions/hid.usb0/protocol
 echo 1 > functions/hid.usb0/subclass
 echo 8 > functions/hid.usb0/report_length
 echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
-C=1
-mkdir -p configs/c.$C/strings/0x409
-echo "Config $C: ECM network" > configs/c.$C/strings/0x409/configuration
-echo 250 > configs/c.$C/MaxPower
-ln -s functions/hid.$N configs/c.$C/
+ln -s functions/hid.usb0 configs/c.1/
+
+# mouse
+mkdir -p functions/hid.usb1
+echo 2 > functions/hid.usb1/protocol
+echo 1 > functions/hid.usb1/subclass
+echo 4 > functions/hid.usb1/report_length
+echo -ne \\x05\\x01\\x09\\x02\\xa1\\x01\\x09\\x01\\xa1\\x00\\x05\\x09\\x19\\x01\\x29\\x05\\x15\\x00\\x25\\x01\\x95\\x05\\x75\\x01\\x81\\x02\\x95\\x01\\x75\\x03\\x81\\x01\\x05\\x01\\x09\\x30\\x09\\x31\\x09\\x38\\x15\\x81\\x25\\x7f\\x75\\x08\\x95\\x03\\x81\\x06\\xc0\\xc0 > functions/hid.usb1/report_desc
+ln -s functions/hid.usb1 configs/c.1/
+# End functions
+
 ls /sys/class/udc > UDC
