881c96e88dc36aafb646371a088f466b94d68ba7
Sean McLellan <Oceanswave@users.noreply.github.com>
2018-09-03 12:28:15 -0400

    Add Joystick definition

--- enable_hid.sh
+++ enable_hid.sh
@@ -39,6 +39,16 @@ echo 1 > functions/hid.usb1/subclass
 echo 4 > functions/hid.usb1/report_length
 echo -ne \\x05\\x01\\x09\\x02\\xa1\\x01\\x09\\x01\\xa1\\x00\\x05\\x09\\x19\\x01\\x29\\x05\\x15\\x00\\x25\\x01\\x95\\x05\\x75\\x01\\x81\\x02\\x95\\x01\\x75\\x03\\x81\\x01\\x05\\x01\\x09\\x30\\x09\\x31\\x09\\x38\\x15\\x81\\x25\\x7f\\x75\\x08\\x95\\x03\\x81\\x06\\xc0\\xc0 > functions/hid.usb1/report_desc
 ln -s functions/hid.usb1 configs/c.1/
+
+# joystick
+mkdir -p functions/hid.usb2
+echo 0 > functions/hid.usb2/protocol
+echo 0 > functions/hid.usb2/subclass
+echo 8 > functions/hid.usb2/report_length
+# Descriptor from https://github.com/NordicSemiconductor/ble-sdk-arduino/blob/master/libraries/BLE/examples/ble_HID_template/USD%20HID%20Report%20Descriptor%20-%20Joystick.txt
+echo -ne \\x05\\x01\\x09\\x04\\xa1\\x01\\x85\\x01\\xa1\\x02\\x09\\x32\\x09\\x31\\x09\\x30\\x15\\x00\\x26\\xff\\x00\\x35\\x00\\x46\\xff\\x00\\x75\\x08\\x95\\x03\\x81\\x02\\xc0\\x85\\x02\\xa1\\x02\\x05\\x09\\x29\\x02\\x19\\x01\\x95\\x02\\x75\\x01\\x25\\x01\\x15\\x00\\x81\\x02\\x95\\x01\\x75\\x06\\x81\\x01\\xc0\\xc0 > functions/hid.usb2/report_desc
+ln -s functions/hid.usb2 configs/c.1/
+
 # End functions
 
 # OS descriptors
