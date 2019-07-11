a280aeceb67f2e8668bdf1bb2230690cbb8ef237
Sean McLellan <sean@baristalabs.io>
2019-07-11 12:31:52 -0400

    Update test scripts

--- test-joystick.sh
+++ test-joystick.sh
@@ -3,6 +3,6 @@
 # Test if is Root
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-echo -ne "\x12\x34\x56\x78\0\0\0\0\0\0\0\0\0\0\0" > /dev/hidg2
+echo -ne "\x7f\x7f\x7f\x14" > /dev/hidg2
 sleep 1
-echo -ne "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0" > /dev/hidg2
+echo -ne "\x00\x00\x00\x04" > /dev/hidg2
