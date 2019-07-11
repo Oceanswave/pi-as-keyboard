c50b7be507a01b2bc4fe571bf1c41a75f0271cc5
Sean McLellan <sean@baristalabs.io>
2019-07-11 12:39:04 -0400

    Update Joystick Test

--- test-joystick.sh
+++ test-joystick.sh
@@ -3,6 +3,7 @@
 # Test if is Root
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
+# Full Throttle, Lower Right, Button 1, Neutral Hat.
 echo -ne "\x7f\x7f\x7f\x14" > /dev/hidg2
 sleep 1
 echo -ne "\x00\x00\x00\x04" > /dev/hidg2
